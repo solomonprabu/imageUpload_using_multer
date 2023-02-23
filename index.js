62a3c0ced954d966866de4a22c95c91e89932202
solomonprabu <solomonprabu002@gmail.com>
2023-02-23 16:59:32 +0530

    validation

--- index.js
+++ index.js
@@ -1,31 +1,67 @@
 const express = require("express");
 const app = express();
-const path = require('path')
-const multer = require('multer')
+const path = require("path");
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'Images')
+  destination: (req, file, cb) => {
+    cb(null, "Images");
+  },
+
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(
+      null,
+      file.originalname.replace(/\.[^/.]/, "") +
+        "_" +
+        Date.now() +
+        path.extname(file.originalname)
+    );
+  },
+});
+
+const maxSize = 5 * 1000 * 1000;
+
+let upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: maxSize,
+    fileFilter: function (res, file, cb) {
+      let filetypes = /jpeg,jpg,png/;
+      let mimetype = filetypes.test(file.mimetype);
+      let extname = filetypes.test(
+        path.extname(file.originalname).toLowerCase()
+      );
+
+      if (mimetype && extname) {
+        return cb(null, true);
+      }
+
+      cb(
+        "Error: File upload only supports the following filetypes: " + filetypes
+      );
     },
+  },
+});
 
-    filename: (req, file, cb)=>{
-        console.log(file);
-        cb(null, Date.now() + path.extname(file.originalname))
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("uploads");
+});
+
+app.post("/uploads", upload.single("image"), (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      if (err instanceof multer.MulterError && err.code == "LIMIT_FILE_SIZE") {
+        return res.send("file exceeds the allowed limit");
+      }
+      res.send(err);
+    } else {
+      res.send("uploaded successfully");
     }
-
-});
-
-const upload = multer({storage: storage})
-
-app.set('views', path.join(__dirname, "views")); 
-app.set("view engine","ejs");
-
-app.get("/", (req,res)=>{
-    res.render('uploads');
-});
-
-app.post("/uploads", upload.single('image'), (req,res)=>{
-    res.send("uploaded successfully");
+  });
 });
 
 app.listen(3001);
